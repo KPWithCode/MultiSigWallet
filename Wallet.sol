02354912058f46709b0b563e07cb11e22ef128f3
Kyle Phillips <khillips375@yahoo.com>
2020-05-10 17:32:13 -0400

    Transfer struct

--- Wallet.sol
+++ Wallet.sol
@@ -4,6 +4,15 @@ pragma solidity ^0.6.0;
 contract Wallet {
     address[] public approvers;
     uint public quorum;
+    struct Transfer {
+        uint id;
+        uint amount;
+        address payable to;
+        uint approvals;
+        bool sent;
+    }
+    mapping(uint => Transfer) public transfers;
+    uint public nextId;
 
     constructor(address[] memory _approvers, uint _quorum) public {
         approvers = _approvers;
