d4707a11d0fef45785eb321df43287cccbbbe1df
Kyle Phillips <khillips375@yahoo.com>
2020-05-12 20:29:39 -0400

    Test: Should not create transfer if sender is not approved

--- wallet/test/wallet.js
+++ wallet/test/wallet.js
@@ -1,5 +1,7 @@
+const { expectRevert } = require('@openzeppelin/test-helpers')
 // contract artifact allows you to intgrate with contract
 const Wallet = artifacts.require('Wallet');
+
 // truffle blockchain generates 10 addresses funded with fake ether
 // I access this array of addresses using the accounts argument
 contract('Wallet', (accounts) => {
@@ -30,6 +32,11 @@ contract('Wallet', (accounts) => {
         assert(transfers[0].to === accounts[5]);
         assert(transfers[0].approvals=== '0');
         assert(transfers[0].sent === false);
-
-    })
+    });
+    it('Should not create transfers if sender is not approved', async () => {
+        await expectRevert(
+        wallet.createTransfer(100, accounts[5], {from:accounts[4]}),
+        'only approver is allowed'
+        ); 
+    });
 });
