d21a9c1af8fd56a5abda2c90b14d4243e9d89a3a
Kyle Phillips <khillips375@yahoo.com>
2020-05-12 21:05:43 -0400

    Test to increment approvals

--- wallet/test/wallet.js
+++ wallet/test/wallet.js
@@ -39,4 +39,13 @@ contract('Wallet', (accounts) => {
         'only approver is allowed'
         ); 
     });
+    it('Should increment approvals', async () => {
+        await wallet.createTransfer(100,accounts[5], {from: accounts[0]});
+        await wallet.approveTransfer(0, {from: accounts[0]});
+        const transfers = await wallet.getTransfers();
+        const balance = await web3.eth.getBalance(wallet.address);
+        assert(transfers[0].approvals === '1');
+        assert(transfers[0].sent === false);
+        assert(balance === '1000');
+    });
 });
