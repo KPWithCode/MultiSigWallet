55f93528aeb93f722d7390272b43cebaace1a6f4
Kyle Phillips <khillips375@yahoo.com>
2020-05-14 22:03:22 -0400

    Connecting front end to smart contract with Web3

---
+++ wallet/client/src/utils.js
@@ -0,0 +1,21 @@
+import Web3 from 'web3';
+import Wallet from './contracts/Wallet.json';
+
+const getWeb3 = () => {
+    // truffle develope started at ...
+    // pass url to node that runs development blockchain
+    new Web3('http://localhost:9545')
+};
+// contract instance
+const getWallet = async web3 => {
+    const networkId = await web3.eth.net.getId();
+    const contractDeployment = Wallet.networks[networkId];
+    return new web3.eth.Contract(
+        // abi
+        Wellet.abi,
+        // address of smart contract
+        contractDeployment && contractDeployment.address
+    )
+}
+
+export { getWeb3, getWallet }
