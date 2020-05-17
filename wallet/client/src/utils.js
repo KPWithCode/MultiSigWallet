2011b2c3992a9a9f31840a9a118d9ab6e2a8e999
Kyle Phillips <khillips375@yahoo.com>
2020-05-17 16:07:16 -0400

    getWeb function now integrates web3 with metamask for signing transactions

--- wallet/client/src/utils.js
+++ wallet/client/src/utils.js
@@ -4,7 +4,29 @@ import Wallet from './contracts/Wallet.json';
 const getWeb3 = () => {
     // truffle develope started at ...
     // pass url to node that runs development blockchain
-    return new Web3('http://localhost:9545')
+    // accepts transactions that arent signed NOT FOR PRODUCTION
+    // return new Web3('http://localhost:9545')
+    // *************
+    // intergrate for metamask where I need to sign a transactiom(mainnet)
+    return new Promise((resolve, reject) => {
+        window.addEventListener('load', async() => {
+            if (window.ethereum) {
+                const web3 = new Web3(window.ethereum);
+                try {
+                    await window.ethereum.enable();
+                } catch (err) {
+                    reject(err)
+                }
+    
+            } else if (window.web3) {
+                // old metamask
+                resolve(window.web3)
+            } else {
+                // no metamask
+                reject('Must install Metamask')
+            }
+        })
+    })
 };
 // contract instance
 const getWallet = async web3 => {
