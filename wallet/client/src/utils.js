50ae09ad73f3670ef7f2b80cfd977aa2996b3f4b
Kyle Phillips <khillips375@yahoo.com>
2020-05-24 13:29:12 -0400

    Minor changes

--- wallet/client/src/utils.js
+++ wallet/client/src/utils.js
@@ -2,42 +2,51 @@ import Web3 from 'web3';
 import Wallet from './contracts/Wallet.json';
 
 const getWeb3 = () => {
-    // truffle develope started at ...
-    // pass url to node that runs development blockchain
-    // accepts transactions that arent signed NOT FOR PRODUCTION
+    // pass url to node that runs development blockchain. Accepts transactions that arent signed NOT FOR PRODUCTION
     // return new Web3('http://localhost:9545')
     // *************
-    // intergrate for metamask where I need to sign a transactiom(mainnet)
+    // integrate metamask when I need to sign a transaction(mainnet)
     return new Promise((resolve, reject) => {
-        window.addEventListener('load', async() => {
+        // avoids race conditions with web3 injection timing
             if (window.ethereum) {
                 const web3 = new Web3(window.ethereum);
                 try {
-                    await window.ethereum.enable();
-                } catch (err) {
+                    window.ethereum.enable();
+                    resolve(web3)
+                } catch (err) { 
                     reject(err)
                 }
-    
-            } else if (window.web3) {
+            } else if(window.web3) {
                 // old metamask
-                resolve(window.web3)
-            } else {
-                // no metamask
-                reject('Must install Metamask')
+                    resolve(window.web3)
+            } 
+
+    // Fallback to localhost: DEVELOPMENT
+            else {
+                const provider = new Web3.providers.HttpProvider(
+                "http://localhost:9545"
+                );
+                const web3 = new Web3(provider);
+                console.log("No web3 instance injected, using Local web3.");
+                resolve(web3);
             }
-        })
+            // ******************ALTERNATE***********************
+            // else {
+            //     // no metamask
+            //     reject('You must install Metamask')
+            // }
     })
 };
 // contract instance
 const getWallet = async web3 => {
     const networkId = await web3.eth.net.getId();
-    const contractDeployment = Wallet.networks[networkId];
+    const deployedNetwork = Wallet.networks[networkId];
     return new web3.eth.Contract(
         // abi
         Wallet.abi,
         // address of smart contract
-        contractDeployment && contractDeployment.address
-    )
-}
+        deployedNetwork && deployedNetwork.address
+    );
+};
 
 export { getWeb3, getWallet }
